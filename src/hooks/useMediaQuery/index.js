ede9bbbf4c36fee2477e7d9e0553ff00a19ce80b
Daniela Muller <danielamuller20@gmail.com>
2021-06-16 17:16:47 -0300

    useDebugValue e custom hook useMediaQuery

---
+++ src/hooks/useMediaQuery/index.js
@@ -0,0 +1,29 @@
+import { useDebugValue, useEffect, useState } from 'react';
+
+export const useMediaQuery = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(initialValue);
+
+  useDebugValue(`Query: ${queryValue}`, (name) => {
+    return name + ' modificado';
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
+};
